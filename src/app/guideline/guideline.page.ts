cb7967ae8ac25fb59c271bbef0441eee089ce246
Wafiq <22ftt1516@student.pb.edu.bn>
2024-04-08 11:42:40 +0800

    added guideline

--- src/app/guideline/guideline.page.ts
+++ src/app/guideline/guideline.page.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonCardContent, IonCard, IonCardTitle, IonCardSubtitle, IonIcon } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
-import { addIcons } from 'ionicons';
-import { arrowBackCircleOutline } from 'ionicons/icons'; // Changed from 'arrow-back-circle-outline' to 'arrowBackCircleOutline'
 
 @Component({
   selector: 'app-guideline',
@@ -17,7 +15,7 @@ export class GuidelinePage implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { 
   }
 
 }
